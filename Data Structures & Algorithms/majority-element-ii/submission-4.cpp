f1f7e58b525ccbeacf382b089eb7adcd1f4ade92
ishu1225 <143313207+ishu1225@users.noreply.github.com>
2026-04-21 11:45:46 +0530

    Add: majority-element-ii - submission-4

---
+++ Data Structures & Algorithms/majority-element-ii/submission-4.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int candiA;
+        int candiB;
+        int countA=0;
+        int countB=0;
+        int req=nums.size()/3;
+        for(int a:nums){
+            if(a==candiA)countA++;
+            else if(a==candiB)countB++;
+            else  if (countA==0){
+                candiA=a;
+                countA=1;
+            }
+            else if (countB==0){
+                candiB=a;
+                countB=1;
+            }
+            else{
+                countA--;
+                countB--;
+
+            }
+
+        }
+        countA=0;
+        countB=0;
+        for(int a:nums){
+            if(a==candiA) countA++;
+            if(a==candiB) countB++;
+        }
+        vector<int> res;
+
+        if(countA>req) res.push_back(candiA);
+        if(countB>req) res.push_back(candiB);
+        return res;
+    }
+};
