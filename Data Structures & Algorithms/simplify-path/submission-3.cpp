1450bdea02db2ef6b995bb469b1879a3c5bb1cfb
ishu1225 <143313207+ishu1225@users.noreply.github.com>
2026-04-27 09:57:43 +0530

    Add: simplify-path - submission-3

---
+++ Data Structures & Algorithms/simplify-path/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    vector<string> split(string path) {
+        vector<string> result;
+        stringstream ss(path);
+        string token;
+
+        while (getline(ss, token, '/')) {
+            result.push_back(token);
+        }
+
+        return result;
+    }
+    string simplifyPath(string path) {
+        stack<string> st;
+        vector<string> res=split(path);
+        for(int i=0;i<res.size();i++){
+            string temp=res[i];
+            if(temp=="")continue;
+            if(temp==".")continue;
+            if(temp==".."){
+                if(st.empty())continue;
+                st.pop();
+                continue;
+                }
+            st.push(temp);
+        }
+        string result="";
+  
+        stack<string> st2;
+        while(!st.empty()){
+           result="/"+st.top()+result;
+            st.pop();
+        }
+        
+        return result.empty()?"/":result;
+    }
+};
