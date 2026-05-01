89695471416d626c9635337c7bee0172f0ef1b47
ishu1225 <143313207+ishu1225@users.noreply.github.com>
2026-05-01 10:46:54 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=0;
+        int h=x;
+        int ans=0;
+        while(l<=h){
+            long long mid=l+(h-l)/2;
+        
+            if(mid*mid<=x){
+                ans=mid;
+                l=mid+1;
+                }
+            else h=mid-1;
+
+        }
+        return ans;
+    }
+};
