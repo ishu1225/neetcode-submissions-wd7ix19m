13be91fc2c1870184d26cc34368f8bb1a111b259
ishu1225 <143313207+ishu1225@users.noreply.github.com>
2026-05-03 22:33:58 +0530

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* tail= &dummy;
+        ListNode* curr1=list1;
+        ListNode* curr2=list2;
+
+        while(curr1!=NULL&&curr2!=NULL){
+           if(curr1->val<=curr2->val){
+            tail->next=curr1;
+            curr1=curr1->next;
+           }
+           else{
+            tail->next=curr2;
+            curr2=curr2->next;
+           }
+           tail=tail->next;
+        }
+        if(curr1){
+            tail->next=curr1;
+            
+        }
+        else if(curr2){
+            tail->next=curr2;
+        }
+        return  dummy.next;
+    }
+};
